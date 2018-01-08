20c659b1ed10a4a9b56863ad913cff13b919651c
takumon <inouetakumon@gmail.com>
2018-01-08 17:54:03 +0900

    Rocket.Chatのアカウント画面のスクリーンキャプチャを取得する処理を追加

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const sample = require('./sample.js');
 
 
-sample.getTitle('https://example.com').then(title => console.log(title));
+// sample.getTitle('http://www.cryptedtruth.com/entry/2018/01/07/151705').then(title => console.log(title));
+sample.getNekoProfileOfRocketChat('neko', 'neko')
+.then(image => console.log(image));
--- src/sample.js
+++ src/sample.js
@@ -7,14 +7,76 @@ const puppeteer = require('puppeteer');
  * @return 指定したページのタイトル
  */
 const getTitle = async url => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    timeout: 0
+  });
   const pages = await browser.pages();
   const page = pages[0];
-  await page.goto(url);
+  await page.goto(url, {
+    timeout: 1000000
+  });
   const title = await page.title();
   await browser.close();
   return title;
 };
 
 
+
+/**
+ * 指定したアカウントでRocket.Chatにログインしアカウント画面のクリーンキャプチャーを取得する.
+ *
+ * @param {String} userId Rocket.ChatのユーザID
+ * @param {String} password Rocket.Chatのパスワード
+ * @return 指定したアカウントのRocket.Chatアカウント画面スクリーンキャプチャー
+ */
+const getNekoProfileOfRocketChat = async (userId, password) => {
+  console.log('処理開始');
+  const address = '10.74.85.64';
+  const port = '3001';
+  const rocketChatUrl = 'http://' + address + ':' + port;
+
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    timeout: 0
+  });
+  const pages = await browser.pages();
+  const page = pages[0];
+  await page.goto(rocketChatUrl, {
+    waitUntil: 'networkidle0',
+    timeout: 100000 // = 100sec
+  });
+
+  // ユーザ名とパスワードを入力
+  const $userName = await page.$('#emailOrUsername');
+  const $password = await page.$('#pass');
+  await $userName.type(userId);
+  await $password.type(password);
+
+  // ログイン
+  const $loginBtn = await page.$('button.login');
+  await $loginBtn.click();
+
+  // 画面遷移待ち
+  await page.waitFor('.sidebar__account-label');
+
+  // アカウント画面に遷移
+  const $accountLabel = await page.$('.sidebar__account-label');
+  await $accountLabel.click();
+  const $accountPopoverItem = await page.$('.rc-popover__item[data-id="account"]');
+  await $accountPopoverItem.click()
+
+  // 画面遷移待ち
+  await page.waitFor('.sidebar-flex__title');
+
+  // スクリーンキャプチャ取得
+  const imageBuffer = await page.screenshot({fullPage: true});
+
+  await browser.close();
+  const imageBase64 = imageBuffer.toString('base64');
+  return imageBase64;
+};
+
 exports.getTitle = getTitle;
+exports.getNekoProfileOfRocketChat = getNekoProfileOfRocketChat;
